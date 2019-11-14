b7a1773776006858253e6d8895bb2f05279d6463
gregatgit <gregdd@outlook.com>
2019-11-14 16:28:07 +1000

    make component classless with useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import InfiniteScroll from './InfiniteScrollComponent'
 
 export const App = () => {
   return (
     <div>
         <h1>SCROLLING</h1>
+        <InfiniteScroll />
     </div>
   )
 }
